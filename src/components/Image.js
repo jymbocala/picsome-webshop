4d5cc40cd29256eaac83e01631eba8853384fe1a
Jym Bocala <jym.bocala@gmail.com>
2023-01-11 18:53:51 +1100

    add heart and cart icon to display when Image component is hovered

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,12 +4,17 @@ export default function Image({ className, img }) {
   // console.log(img)
   const [hover, setHover] = useState(false);
 
+  const heartIcon = hover && <i className="ri-heart-line favorite"></i>;
+  const cartIcon = hover && <i className="ri-add-circle-line cart"></i>;
+
   return (
     <div
       className={`${className} image-container`}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
     >
+      {heartIcon}
+      {cartIcon}
       <img src={img.url} className="image-grid" />
     </div>
   );
