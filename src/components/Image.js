56c54648b1bf89ea336231b0e8f19c2497e1a152
Jym Bocala <jym.bocala@gmail.com>
2023-01-11 18:37:01 +1100

    add hover state and handleHover functionality to  Image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Image({ className, img }) {
-  console.log(img)
+  // console.log(img)
+  const [hover, setHover] = useState(false);
+
   return (
-    <div className={`${className} image-container`}>
+    <div
+      className={`${className} image-container`}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
       <img src={img.url} className="image-grid" />
     </div>
   );
