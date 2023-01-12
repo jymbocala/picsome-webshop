f288b52e0dbdf9995339309a348d4b6b4e06c6f6
Jym Bocala <jym.bocala@gmail.com>
2023-01-12 15:54:02 +1100

    add Cart page setup & create CartItem component

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,23 @@
-import React from "react"
+import React, { useContext } from "react";
+import { Context } from "../Context";
+import CartItem from "../components/CartItem";
 
 export default function Cart() {
-    return (
-        <main className="cart-page">
-            <h1>Check out</h1>
-        </main>
-    )
+  // get cartItems array state from context
+  const {cartItems} = useContext(Context);
+  // map over each cart item and pass the item as a prop for a CartItem component
+  const cartItemElements = cartItems.map(item => (
+    <CartItem key={item.id} item={item}/>
+  ));
+
+  return (
+    <main className="cart-page">
+      <h1>Check out</h1>
+      {cartItemElements}
+      <p className="total-cost">Total: </p>
+      <div className="order-button">
+        <button>Place Order</button>
+      </div>
+    </main>
+  );
 }
